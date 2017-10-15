466304e4e5e40065ff650b2bd093b306b3f0a583
Evgeny Karpenko <johnny_headinair@mail.ru>
2017-10-15 21:25:05 +0700

    Lesson_13_v2

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket < ApplicationRecord
-  validates :number, presence: true
-
-  belongs_to :train, optional: true
-  belongs_to :from_station, class_name: 'RailwayStation', optional: true
-  belongs_to :to_station, class_name: 'RailwayStation', optional: true
+  belongs_to :train
+  belongs_to :from_station, class_name: 'RailwayStation'
+  belongs_to :to_station, class_name: 'RailwayStation'
   belongs_to :user, optional: true
+
+  validates :number, presence: true
 end
