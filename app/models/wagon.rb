9d7c30c0c42f858898c27923ed538f0981fad27d
Evgeny Karpenko <johnny_headinair@mail.ru>
2017-10-28 17:15:38 +0700

    fix_syntax

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -2,7 +2,6 @@ class Wagon < ApplicationRecord
   belongs_to :train
   has_many :tickets
 
-  validates :variety, :upper_seats, :lower_seats, presence: true
-  validates :variety, inclusion: { in: %w(Купейный Плацкартный) }
-  validates :upper_seats, :lower_seats, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :variety, presence: true, inclusion: { in: %w(Купейный Плацкартный) }
+  validates :upper_seats, :lower_seats, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
