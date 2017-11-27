df3bd7e8e07e5c0430745aa60b563dfdb6ad82fc
Evgeny Karpenko <johnny_headinair@mail.ru>
2017-11-27 22:29:52 +0700

    with_error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admin_routes_path
+    else
+      search_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  resources :trains do
-    resources :wagons, shallow: true
-    resources :tickets, shallow: true
-  end
-  resources :railway_stations do
-    patch :update_position, on: :member
-    patch :update_arrival_time, on: :member
-    patch :update_departure_time, on: :member
-  end
-  resources :routes
-  resources :tickets
-  resources :users
-  resources :wagons
+  devise_for :users
+
+  resources :tickets, only: [:create, :show]
   resource :search, only: [:show]
+
+  namespace :admin do
+    resources :trains do
+      resources :wagons, shallow: true
+      resources :tickets, shallow: true
+    end
+    resources :railway_stations do
+      patch :update_position, on: :member
+      patch :update_arrival_time, on: :member
+      patch :update_departure_time, on: :member
+    end
+    resources :routes
+  end
 
   get 'welcome/index'
 
