534ddc045c429f6523adf9cd30002ff6a49fb6ef
Evgeny Karpenko <johnny_headinair@mail.ru>
2017-12-09 16:02:00 +0700

    lesson_19

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,15 +3,14 @@ class Route < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
 
-  validates :name, presence: true
   validate :stations_count
 
-  before_validation :set_name
+  before_save :set_name
 
   private
 
   def set_name
-    self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+    self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}" if name.blank?
   end
 
   def stations_count
